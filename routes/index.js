a5262211bec252b0211a31a2a4e213a681250d80
MP <mm.potapov@mail.ru>
2023-03-23 17:46:48 +0300

    feat: Joi validation

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,45 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const { createUser, login } = require('../controllers/users');
 const auth = require('../middlewares/auth');
+const { regexURL } = require('../utils/validation');
+const NotFoundError = require('../errors/notFoundError');
 
-const {
-  NOT_FOUND,
-} = require('../utils/httpStatus');
-
-/** Импорт роутеров на /users и /cards */
+/** Импорт роутеров на /users и /cards с Joi валидацией */
 const usersRouter = require('./users');
 const cardsRouter = require('./cards');
 
-/** Обработка запросов на /signup и /signin */
-router.post('/signup', createUser);
-router.post('/signin', login);
+/** Обработка запросов на /signup и /signin с Joi валидацией */
+router.post(
+  '/signup',
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+      avatar: Joi.string().pattern(regexURL),
+      email: Joi.string().required().email(),
+      password: Joi.string().required(),
+    }),
+  }),
+  createUser,
+);
+
+router.post(
+  '/signin',
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    }),
+  }),
+  login,
+);
 
 /** Обработка запросов на /users, /cards и все остальные адреса */
 router.use('/users', auth, usersRouter);
 router.use('/cards', auth, cardsRouter);
-router.use('*', auth, (req, res) => {
-  res.status(NOT_FOUND).send({ message: 'Страница не найдена' });
+router.use('*', auth, () => {
+  throw new NotFoundError('Страница не найдена');
 });
 
 module.exports = router;
